456de16ac396b9adfdec3dd9db5724bf0ff217e7
Roger Roig <roger.roigrod@gmail.com>
2019-03-12 15:21:30 +0100

    finished challenge 2

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -7,32 +7,38 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+import random
+import string
+import sys
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+characters = string.ascii_lowercase + string.digits
+
+
+def random_string_generator(length, char_seq):
+    result_string = ''
+    for i in range(length):
+        result_string += random.choice(char_seq)
+    return result_string
+
+
+def batch_string_generator(str_num, min_len=8, max_len=12):
+    string_list = []
+    for i in range(str_num):
+        if min_len < max_len:
+            c = random.choice(range(min_len, max_len+1))
+        elif min_len == max_len:
+            c = min_len
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+        string_list.append(random_string_generator(c, characters))
+    return string_list
+
 
 a = input('Enter minimum string length: ')
 b = input('Enter maximum string length: ')
 n = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+if a not in string.digits or b not in string.digits or n not in string.digits:
+    sys.exit('Incorrect input. Please try again.')
+
+print(batch_string_generator(int(n), int(a), int(b)))
